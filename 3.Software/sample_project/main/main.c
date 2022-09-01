ee5d7025e88eb7c8a0487e99fa46feb0a1cfb66e
NUOZHIHUI <1285574579@qq.com>
2022-09-01 20:34:34 +0800

    V0.5

--- 3.Software/sample_project/main/main.c
+++ 3.Software/sample_project/main/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h" //RTOS实时系统
+#include "freertos/task.h"     //RTOS实时系统
+#include "esp_system.h"
+#include <stdio.h>
+#include "./Peripheral/peripheral.h"
 
 void app_main(void)
 {
 
+    BEEP_Inint();
+
+    while (1)
+    {
+        //BEEP
+        ON_BEEP(1000);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        OFF_BEEP();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
